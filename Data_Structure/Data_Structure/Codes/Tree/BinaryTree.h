48c3347b0c9f3f90092bf5d0aaa4f6a40f1eca47
feudalsociety <32698816+feudalsociety@users.noreply.github.com>
2017-10-28 21:34:02 +0900

    commit

---
+++ Data_Structure/Data_Structure/Codes/Tree/BinaryTree.h
@@ -0,0 +1,19 @@
+#pragma once
+typedef int BTData;
+
+typedef struct _bTreeNode
+{
+	BTData data;
+	struct _bTreeNode * left;
+	struct _bTreeNode * right;
+} BTreeNode;
+
+BTreeNode * makeBTreeNode(void);
+BTData GetData(BTreeNode * bt);
+void SetData(BTreeNode * bt, BTData data);
+
+BTreeNode * GetLeftSubTree(BTreeNode * bt);
+BTreeNode * GetRightSubTree(BTreeNode * bt);
+
+void MakeLeftSubTree(BTreeNode * main, BTreeNode * sub);
+void MakeRightSubTree(BTreeNode * main, BTreeNode * sub);
